f2ce861c3d72480ea3ec728a2a3312caafd34a54
Hemant Medhsia <hemantmedhsia@gmail.com>
2025-08-17 21:53:38 +0530

    adding config and security

--- src/main/java/com/hemant/springsecurity/controller/SecurityTest.java
+++ src/main/java/com/hemant/springsecurity/controller/SecurityTest.java
@@ -8,17 +8,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping
 public class SecurityTest {
-    @GetMapping
+    @GetMapping("public/view")
     public String test(HttpServletRequest request) {
         return "Welcome to <h2>Spring Security</h2>. You are now <h1>Authenticated</h1>." + request.getSession().getId();
     }
 
-    @GetMapping("csrf-token")
+    @GetMapping("/open/view")
+    public String open(HttpServletRequest request) {
+        return "Welcome to <h2>Open Security</h2>. You are now <h1>Able to Inter openly</h1>." + request.getSession().getId();
+    }
+
+    @GetMapping("csrf-token/view")
     public CsrfToken getCsrfToken(HttpServletRequest request) {
         return (CsrfToken) request.getAttribute("_csrf");
     }
 
-    @PostMapping
+    @PostMapping("open/post")
     public String postTesting(@RequestBody String H) {
         return H;
     }
